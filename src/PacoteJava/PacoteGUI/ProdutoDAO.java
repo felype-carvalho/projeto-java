075b577233d419f83f82ed2a4d3466217944c3ba
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-06-18 22:11:29 -0300

    classe PDAO foi atualizada

--- src/PacoteJava/PacoteGUI/ProdutoDAO.java
+++ src/PacoteJava/PacoteGUI/ProdutoDAO.java
@@ -4,5 +4,19 @@ import java.sql.*;
 import java.util.*;
 
 public class ProdutoDAO {
-    
+
+    private Connection con;
+
+    public ProdutoDAO(Connection con) {
+        setCon(con);
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
 }
