c5d987c2fe62817245dda9d9213ba47947c06ab9
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-06-18 22:04:59 -0300

    foi adc a classe bean

---
+++ src/PacoteJava/PacoteGUI/Bean.java
@@ -0,0 +1,32 @@
+package PacoteJava.PacoteGUI;
+
+
+public class Bean {
+    private String nome, codigo, categoria, preco; 
+    
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCodigo(){
+        return codigo;
+    }
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    public String getCategoria(){
+        return categoria;
+    }
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+    public String getPreco(){
+        return preco;
+    }
+    public void setPreco(String preco){
+        this.preco = preco;
+    }
+}
+
